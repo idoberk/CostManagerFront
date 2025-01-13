c8f09b8d0d88365859804924c0840b667f50c7f0
Ido Berkovits <berkovitsido@gmail.com>
2025-01-13 14:48:10 +0200

    Implemented indexedDB wrapper library

--- idb.js
+++ idb.js
@@ -28,11 +28,64 @@ costManagerUtils.Database = class {
 						keyPath: 'id',
 						autoIncrement: true
 					});
-					
+
 					costStore.createIndex('dateIndex', 'date');
 					costStore.createIndex('categoryIndex', 'category');
 				}
 			};
 		});
 	}
-}
+
+	addCostItem(costData) {
+		return new Promise((res, rej) => {
+			const transaction = this.db.transaction(['costs'], 'readwrite');
+			const store = transaction.objectStore('costs');
+			const addRequest = store.add(costData);
+
+			addRequest.onsuccess = () => res(addRequest.result);
+			addRequest.onerror = () => rej(new Error('Failed to add cost item'));
+		});
+	}
+
+	getMonthlyCosts(month, year) {
+		return new Promise((res, rej) => {
+			const transaction = this.db.transaction(['costs'], 'readonly');
+			const store = transaction.objectStore('costs');
+			const costs = [];
+			const cursorRequest = store.openCursor();
+
+			cursorRequest.onsuccess = (event) => {
+				const cursor = event.target.result;
+
+				if (cursor) {
+					const item = cursor.value;
+					const itemDate = new Date(item.date);
+
+					if (itemDate.getFullYear() === year && itemDate.getMonth() === month - 1) {
+						costs.push(item);
+					}
+					cursor.continue();
+				} else {
+					res(costs);
+				}
+			};
+
+			cursorRequest.onerror = () => rej(new Error('Failed to get costs'));
+		});
+	}
+
+	getCategoryTotals(month, year) {
+		return new Promise((res, rej) => {
+			this.getMonthlyCosts(month, year)
+				.then(costs => {
+					const totals = {};
+					costs.forEach((cost) => {
+						totals[cost.category] = (totals[cost.category] || 0) + cost.amount;
+					});
+					res(totals);
+				}).catch(() => rej(new Error('Failed to get category totals')));
+		});
+	}
+};
+
+export default costManagerUtils;
